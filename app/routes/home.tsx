83a64afbb72814ff49893117aa73896d116b46ad
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-01-10 20:51:21 +0100

    feat: initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,17 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    {title: "New React Router App"},
+    {name: "description", content: "Welcome to React Router!"},
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="w-screen h-screen flex items-center justify-center flex-col">
+      <h1 className="font-bold text-7xl"> PawPrint </h1>
+      <h1> Coming Soon </h1>
+    </div>
+  );
 }
