209c3d261f76b96a7f86c0ca7960a5c484bb1134
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-02-06 20:27:10 +0100

    feat: working pet form

--- src/components/CreateShelterPetListing/CreateForm.vue
+++ src/components/CreateShelterPetListing/CreateForm.vue
@@ -1,6 +1,6 @@
 <template>
   <Form
-    v-slot="{ meta, values, validate }"
+    v-slot="{ meta, values, validate, errors }"
     as=""
     keep-values
     :validation-schema="toTypedSchema(formSchema[stepIndex - 1])"
@@ -15,8 +15,6 @@
           (e) => {
             e.preventDefault()
             validate()
-            console.log(validate())
-
             if (stepIndex === steps.length && meta.valid) {
               onSubmit(values)
             }
@@ -71,307 +69,12 @@
 
         <div class="grid px-16 mt-4 gap-y-2 gap-x-4 gird-cols-1 md:grid-cols-2">
           <template v-if="stepIndex === 1">
-            <div class="flex flex-col w-full gap-4">
-              <FormField v-slot="{ componentField }" name="name">
-                <FormItem v-auto-animate>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input type="text" placeholder="Iris" v-bind="componentField" />
-                  </FormControl>
-                  <FormDescription> The pet's name </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ componentField }" name="breed">
-                <FormItem v-auto-animate>
-                  <FormLabel>Breed</FormLabel>
-                  <FormControl>
-                    <Input type="text" placeholder="Bulldog" v-bind="componentField" />
-                  </FormControl>
-                  <FormDescription>The pet's breed </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ componentField }" name="ageYears">
-                <FormItem v-auto-animate>
-                  <FormLabel>Age</FormLabel>
-                  <FormControl>
-                    <Input type="text" placeholder="Example: 2" v-bind="componentField" />
-                  </FormControl>
-                  <FormDescription> The pet's age.</FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ componentField }" name="energyLevel">
-                <FormItem v-auto-animate>
-                  <FormLabel>Energy Level</FormLabel>
-                  <FormControl>
-                    <Input type="number" placeholder="Energy between 1-5" v-bind="componentField" />
-                  </FormControl>
-                  <FormDescription>
-                    How would you describe the pet's energy levels. A number between 1-5
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ componentField }" name="petTypeId">
-                <FormItem v-auto-animate>
-                  <FormLabel>Pet Type</FormLabel>
-                  <Select v-bind="componentField">
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select the type of your pet" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectItem v-for="type in types" :value="type.id" :key="type.id">
-                          {{ type.name }}
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                  <FormDescription> The type of pet you are listing </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ componentField }" name="petSizeId">
-                <FormItem v-auto-animate>
-                  <FormLabel>Pet Size</FormLabel>
-                  <Select v-bind="componentField">
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select the size of your pet" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectItem v-for="size in sizes" :value="size.id" :key="size.id">
-                          {{ size.name }}
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                  <FormDescription> The size of pet you are listing </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ componentField }" name="petGenderId">
-                <FormItem v-auto-animate>
-                  <FormLabel>Pet Gender</FormLabel>
-                  <Select v-bind="componentField">
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select the gender of your pet" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectItem v-for="gender in genders" :value="gender.id" :key="gender.id">
-                          {{ gender.name }}
-                        </SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                  <FormDescription> The size of pet you are listing </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ componentField }" name="specialRequirements">
-                <FormItem>
-                  <FormLabel>Special Requirements (Optional)</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Any special requirements for the pet"
-                      class="h-10 resize-none"
-                      v-bind="componentField"
-                    />
-                  </FormControl>
-                  <FormDescription> Special requirements specific to the pet </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ componentField }" name="behavioralNotes">
-                <FormItem>
-                  <FormLabel>Behavioral Notes (Optional)</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Any behavioral notes for the pet"
-                      class="resize-none"
-                      v-bind="componentField"
-                    />
-                  </FormControl>
-                  <FormDescription> Behavioral notes for the pet </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              </FormField>
-            </div>
-            <div class="flex flex-col w-full gap-4">
-              <FormField v-slot="{ handleBlur, handleChange, value }" name="avatarImg">
-                <Card class="w-full rounded-none border-accent">
-                  <CardHeader>
-                    <CardTitle> Pet Avatar </CardTitle>
-                    <CardDescription> The main image of the listing </CardDescription>
-                  </CardHeader>
-
-                  <CardContent>
-                    <div class="flex flex-col gap-2">
-                      <AspectRatio v-if="value" :ratio="16 / 9">
-                        <img
-                          :src="createURLPreview(value)"
-                          alt="Pet Avatar"
-                          class="object-cover w-full h-full rounded-md"
-                        />
-                      </AspectRatio>
-
-                      <FormItem v-auto-animate>
-                        <FormControl>
-                          <Input
-                            type="file"
-                            accept="image/*"
-                            @change="handleChange"
-                            @blur="handleBlur"
-                          />
-                        </FormControl>
-                      </FormItem>
-                    </div>
-                  </CardContent>
-                </Card>
-              </FormField>
-
-              <FormField
-                v-slot="{ handleBlur, handleChange, value, setValue }"
-                name="imageShowcase"
-              >
-                <Card class="w-full rounded-none border-accent">
-                  <CardHeader>
-                    <div class="flex flex-row items-center justify-between">
-                      <div class="flex flex-col gap-2">
-                        <CardTitle> Image Showcase </CardTitle>
-                        <CardDescription> Additional images for the listing </CardDescription>
-                      </div>
-
-                      <Button
-                        v-if="value && value.length > 0"
-                        size="sm"
-                        variant="outline"
-                        @click="triggerAddFileInput"
-                      >
-                        <Plus class="w-8 h-8" />
-                      </Button>
-                      <input
-                        ref="addImageInputRef"
-                        type="file"
-                        accept="image/*"
-                        @change="
-                          (e: Event) => {
-                            const files = (e.target as HTMLInputElement).files
-
-                            if (files && files.length > 0) {
-                              const newImages = [...value, ...files]
-                              setValue(newImages, true)
-                            }
-
-                            console.log(value)
-                          }
-                        "
-                        @blur="
-                          (e: Event) => {
-                            console.log('blur')
-                          }
-                        "
-                        style="display: none"
-                      />
-                    </div>
-                  </CardHeader>
-
-                  <CardContent>
-                    <div class="flex flex-col gap-2">
-                      <FormItem v-auto-animate>
-                        <div v-if="value" class="flex flex-col w-full gap-2">
-                          <div
-                            v-for="image in value"
-                            :key="image"
-                            class="flex flex-row items-center justify-between p-4 border"
-                          >
-                            <img
-                              :src="createURLPreview(image)"
-                              alt="Pet Avatar"
-                              class="object-cover w-24 h-24"
-                            />
-                            <Button
-                              size="sm"
-                              variant="outline"
-                              @click="
-                                () => {
-                                  const withoutImage = value.filter((file: File) => file !== image)
-                                  setValue(withoutImage, true)
-                                }
-                              "
-                            >
-                              Remove
-                            </Button>
-                          </div>
-                        </div>
-                        <FormControl>
-                          <Input
-                            type="file"
-                            accept="image/*"
-                            multiple
-                            @change="handleChange"
-                            @blur="handleBlur"
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    </div>
-                  </CardContent>
-                </Card>
-              </FormField>
-
-              <FormField v-slot="{ value, handleChange }" name="goodWithDogs">
-                <FormItem class="flex flex-row items-center justify-between p-4 border rounded-lg">
-                  <div class="flex flex-row items-center gap-4">
-                    <Dog />
-                    <div class="space-y-0.5">
-                      <FormLabel class="text-base"> Good with Dogs </FormLabel>
-                      <FormDescription> Is the pet usually good with dogs ? </FormDescription>
-                    </div>
-                  </div>
-                  <FormControl>
-                    <Switch :checked="value" @update:checked="handleChange" />
-                  </FormControl>
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ value, handleChange }" name="goodWithCats">
-                <FormItem class="flex flex-row items-center justify-between p-4 border rounded-lg">
-                  <div class="flex flex-row items-center gap-4">
-                    <Cat class="w-6 h-6" />
-                    <div class="space-y-0.5">
-                      <FormLabel class="text-base"> Good with Cats </FormLabel>
-                      <FormDescription> Is the pet usually good with cats ? </FormDescription>
-                    </div>
-                  </div>
-                  <FormControl>
-                    <Switch :checked="value" @update:checked="handleChange" />
-                  </FormControl>
-                </FormItem>
-              </FormField>
-              <FormField v-slot="{ value, handleChange }" name="goodWithChildren">
-                <FormItem class="flex flex-row items-center justify-between p-4 border rounded-lg">
-                  <div class="flex flex-row items-center gap-4">
-                    <Baby />
-                    <div class="space-y-0.5">
-                      <FormLabel class="text-base"> Good with Children </FormLabel>
-                      <FormDescription> Is the pet usually good with kids ? </FormDescription>
-                    </div>
-                  </div>
-                  <FormControl>
-                    <Switch :checked="value" @update:checked="handleChange" />
-                  </FormControl>
-                </FormItem>
-              </FormField>
-            </div>
+            <CreatePetForm
+              :genders="genders"
+              :sizes="sizes"
+              :types="types"
+              :health-statuses="healthStatuses"
+            />
           </template>
 
           <template v-if="stepIndex === 2"> </template>
@@ -403,25 +106,7 @@
 
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
+import { Form } from '@/components/ui/form'
 import {
   Stepper,
   StepperDescription,
@@ -430,59 +115,44 @@ import {
   StepperTitle,
   StepperTrigger,
 } from '@/components/ui/stepper'
-import { Switch } from '@/components/ui/switch'
-import { Textarea } from '@/components/ui/textarea'
 import type { PetGender } from '@/types/models/petGender'
 import type { PetSize } from '@/types/models/petSize'
 import type { PetType } from '@/types/models/petType'
 
 import { toTypedSchema } from '@vee-validate/zod'
-import { Baby, Cat, Check, Circle, Dog, Dot, Plus } from 'lucide-vue-next'
+import { Check, Circle, Dot } from 'lucide-vue-next'
 import { ref } from 'vue'
 import * as z from 'zod'
-import { AspectRatio } from '../ui/aspect-ratio'
-
-const MAX_FILE_SIZE = 500000
+import CreatePetForm from './CreatePetForm.vue'
+import type { HealthStatus } from '@/types/models/healthStatus'
+const MAX_FILE_SIZE = 5000000
 const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp']
-
-const addImageInputRef = ref<HTMLInputElement | null>(null)
-
-const triggerAddFileInput = () => {
-  if (addImageInputRef.value) {
-    console.log(addImageInputRef)
-    addImageInputRef.value.click()
-  }
-}
 
 const formSchema = [
   z.object({
     name: z.string().min(2).max(50),
     breed: z.string().min(2).max(50).nullable().default(null),
     avatarImg: z
-      .any()
-      .refine((files) => files?.length == 1, 'Image is required.')
-      .refine((files) => files?.[0]?.size <= MAX_FILE_SIZE, `Max file size is 5MB.`)
+      .custom<File>()
+      .refine((file) => file.size <= MAX_FILE_SIZE, `File must be less than or equal to 5MB.`)
       .refine(
-        (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
-        '.jpg, .jpeg, .png and .webp files are accepted.',
+        (file) => ACCEPTED_IMAGE_TYPES.includes(file.type),
+        'Only .jpg, .jpeg, .png, and .webp files are accepted.',
       ),
-    imageShowcase: z.array(
-      z
-        .custom<FileList>() // Use FileList for HTML file input
-        .refine((files) => files?.length > 0, 'At least one image is required.')
-        .refine((files) => files?.length <= 10, 'You can upload a maximum of 10 files.')
-        .refine((files) => {
-          return Array.from(files).every((file) => file.size <= MAX_FILE_SIZE)
-        }, `Each file must be less than or equal to 5MB.`)
-        .refine((files) => {
-          return Array.from(files).every((file) => ACCEPTED_IMAGE_TYPES.includes(file.type))
-        }, 'Only .jpg, .jpeg, .png, and .webp files are accepted.'),
-    ),
+    imageShowcase: z
+      .custom<FileList>() // Use FileList for HTML file input
+      .refine((files) => files?.length > 0, 'At least one image is required.')
+      .refine((files) => files?.length <= 10, 'You can upload a maximum of 10 files.')
+      .refine((files) => {
+        return Array.from(files).every((file) => file.size <= MAX_FILE_SIZE)
+      }, `Each file must be less than or equal to 5MB.`)
+      .refine((files) => {
+        return Array.from(files).every((file) => ACCEPTED_IMAGE_TYPES.includes(file.type))
+      }, 'Only .jpg, .jpeg, .png, and .webp files are accepted.'),
     ageYears: z.coerce.number().gt(0).lt(100),
     petTypeId: z.string(),
     petGenderId: z.string(),
     petSizeId: z.string(),
-    adoptionStatusId: z.string(),
     healthStatusId: z.string(),
     goodWithChildren: z.boolean().default(false),
     goodWithDogs: z.boolean().default(false),
@@ -490,7 +160,6 @@ const formSchema = [
     energyLevel: z.coerce.number().gt(0).lt(6),
     specialRequirements: z.string().nullable().default(null),
     behaviorialNotes: z.string().nullable().default(null),
-    intakeDate: z.string().nullable().default(null),
   }),
   z.object({
     spayNeuterStatus: z.string(),
@@ -541,11 +210,8 @@ defineProps<{
   types: PetType[]
   sizes: PetSize[]
   genders: PetGender[]
+  healthStatuses: HealthStatus[]
 }>()
-
-const createURLPreview = (file: File) => {
-  return URL.createObjectURL(file)
-}
 
 function onSubmit(values: unknown) {
   console.log(values)
--- src/views/CreateShelterPetListing.vue
+++ src/views/CreateShelterPetListing.vue
@@ -2,9 +2,11 @@
 import CreateForm from '@/components/CreateShelterPetListing/CreateForm.vue'
 import DefaultError from '@/components/Global/DefaultError.vue'
 import DefaultLoader from '@/components/Global/DefaultLoader.vue'
+import healthStatusService from '@/services/healthStatus-service'
 import petGendersService from '@/services/petGender-service'
 import petSizesService from '@/services/petSizes-service'
 import petTypesService from '@/services/petTypes-service'
+import type { HealthStatus } from '@/types/models/healthStatus'
 import type { PetGender } from '@/types/models/petGender'
 import type { PetSize } from '@/types/models/petSize'
 import type { PetType } from '@/types/models/petType'
@@ -42,14 +44,40 @@ const {
   queryFn: petGendersService.getPetGenders,
   retry: 0,
 })
+
+const {
+  data: healthStatusData,
+  isPending: healthStatusQueryIsPending,
+  isError: healthStatusQueryIsError,
+  error: healthStatusQueryError,
+} = useQuery<HealthStatus[]>({
+  queryKey: ['healthStatuses'],
+  queryFn: healthStatusService.getHealthStatuses,
+  retry: 0,
+})
 </script>
 
 <template>
-  <DefaultLoader v-if="petTypeQueryIsPending || petSizeQueryIsPending || petGenderQueryIsPending" />
+  <DefaultLoader
+    v-if="
+      petTypeQueryIsPending ||
+      petSizeQueryIsPending ||
+      petGenderQueryIsPending ||
+      healthStatusQueryIsPending
+    "
+  />
   <DefaultError
-    v-else-if="petTypeQueryIsError || petSizeQueryIsError || petGenderQueryIsError"
+    v-else-if="
+      petTypeQueryIsError ||
+      petSizeQueryIsError ||
+      petGenderQueryIsError ||
+      healthStatusQueryIsError
+    "
     :error="
-      petTypeQueryError?.message || petSizeQueryError?.message || petGenderQueryError?.message
+      petTypeQueryError?.message ||
+      petSizeQueryError?.message ||
+      petGenderQueryError?.message ||
+      healthStatusQueryError?.message
     "
   />
   <CreateForm
@@ -57,5 +85,6 @@ const {
     :types="petTypeData"
     :sizes="petSizeData"
     :genders="petGenderData"
+    :healthStatuses="healthStatusData"
   />
 </template>
